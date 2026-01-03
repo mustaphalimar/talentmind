4a2578d31e7126640ca8912de6f619521abaa02e
Mustapha Limar <tapham079@gmail.com>
2026-01-03 19:00:31 +0100

    fixed header

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -1,12 +1,46 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Container from "../layouts/container";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import Logo from "./logo";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isHeroScrolled, setIsHeroScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+
+      if (window.scrollY > 785) {
+        setIsHeroScrolled(true);
+      } else {
+        setIsHeroScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="fixed top-2 left-0 right-0 z-50 bg-transparent">
+    <header
+      className={cn(
+        "fixed top-0 p-2 left-0 right-0 z-50 transition-all duration-300",
+        {
+          "bg-white/95 bg-opacity-70 backdrop-blur-sm shadow-md": isScrolled,
+          "bg-transparent": !isScrolled,
+        }
+      )}
+    >
       <Container>
         <nav className="">
           <div className="flex items-center justify-between">
@@ -16,22 +50,36 @@ const Header = () => {
             </div>
 
             {/* Navigation */}
-            <div className="flex items-center gap-8">
+            <div
+              className={cn("flex items-center gap-8", {
+                "text-black": isHeroScrolled,
+                "text-white": !isHeroScrolled,
+              })}
+            >
               <a
                 href="#about"
-                className="text-white hover:text-gray-300 transition"
+                className={cn(" transition", {
+                  "hover:text-gray-300": isScrolled && !isHeroScrolled,
+                  "hover:text-gray-500": isHeroScrolled,
+                })}
               >
                 À propos
               </a>
               <a
                 href="#pillars"
-                className="text-white hover:text-gray-300 transition"
+                className={cn(" transition", {
+                  "hover:text-gray-300": isScrolled && !isHeroScrolled,
+                  "hover:text-gray-500": isHeroScrolled,
+                })}
               >
                 Nos piliers
               </a>
               <a
-                href="#schedule"
-                className="text-white hover:text-gray-300 transition"
+                href="#solutions"
+                className={cn(" transition", {
+                  "hover:text-gray-300": isScrolled && !isHeroScrolled,
+                  "hover:text-gray-500": isHeroScrolled,
+                })}
               >
                 Solutions
               </a>
