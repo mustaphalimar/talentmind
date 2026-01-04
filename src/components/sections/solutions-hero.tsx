f6dcfee30ca40125c151eab0bc6802184f794db3
Mustapha Limar <tapham079@gmail.com>
2026-01-05 00:20:01 +0100

    hero images performance improved

--- src/components/sections/solutions-hero.tsx
+++ src/components/sections/solutions-hero.tsx
@@ -6,11 +6,18 @@ const SolutionsHero = () => {
     <section className="min-h-screen flex items-center relative">
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
+        {/* Mobile Background Image */}
         <div
-          className="absolute inset-0 bg-cover bg-center"
+          className="absolute inset-0 bg-cover bg-center md:hidden"
           style={{
-            backgroundImage:
-              "url('https://plus.unsplash.com/premium_photo-1661270491807-534a565e6ff8?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+            backgroundImage: "url('/section-hero-bg-mobile.webp')",
+          }}
+        />
+        {/* Desktop Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center hidden md:block"
+          style={{
+            backgroundImage: "url('/section-hero-bg-desktop.webp')",
           }}
         />
         <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-black/50" />
